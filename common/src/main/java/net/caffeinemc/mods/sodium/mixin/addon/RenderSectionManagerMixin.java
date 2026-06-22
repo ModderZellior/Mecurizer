f5f007ed7280813b8f6f691efe2d2e62d74e97d2
ModderZellior <gersom.rode@gmail.com>
2026-06-22 12:27:18 +0200

    Reduce upload fraction to 0.06f and min budget to 1ms to smooth chunk load stutters

--- common/src/main/java/net/caffeinemc/mods/sodium/mixin/addon/RenderSectionManagerMixin.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/addon/RenderSectionManagerMixin.java
@@ -5,7 +5,9 @@ import net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderSectionManager.class)
@@ -17,5 +19,15 @@ public class RenderSectionManagerMixin {
         if (!MercurizerRuntimePolicy.shouldAnimateVisibleTexturesThisFrame(this.lastFrameDuration)) {
             ci.cancel();
         }
+    }
+
+    @ModifyConstant(method = "updateChunks", constant = @Constant(floatValue = 0.1f, ordinal = 0))
+    private float modifyUploadFraction(float ignored) {
+        return 0.06f;
+    }
+
+    @ModifyConstant(method = "updateChunks", constant = @Constant(longValue = 2_000_000L, ordinal = 0))
+    private long modifyMinUploadBudget(long ignored) {
+        return 1_000_000L;
     }
 }
