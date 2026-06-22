829fbf5a9bc592d8050ca0aec0d394776d738f43
ModderZellior <gersom.rode@gmail.com>
2026-06-22 11:52:06 +0200

    Fix chunk holes: remove collectChunkBuildResults cap that dropped dequeued results

--- common/src/main/java/net/caffeinemc/mods/sodium/mixin/addon/RenderSectionManagerMixin.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/mixin/addon/RenderSectionManagerMixin.java
@@ -1,18 +1,12 @@
 package net.caffeinemc.mods.sodium.mixin.addon;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.caffeinemc.mods.sodium.client.MercurizerRuntimePolicy;
 import net.caffeinemc.mods.sodium.client.render.chunk.RenderSectionManager;
-import net.caffeinemc.mods.sodium.client.render.chunk.compile.BuilderTaskOutput;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.ArrayList;
 
 @Mixin(RenderSectionManager.class)
 public class RenderSectionManagerMixin {
@@ -23,24 +17,5 @@ public class RenderSectionManagerMixin {
         if (!MercurizerRuntimePolicy.shouldAnimateVisibleTexturesThisFrame(this.lastFrameDuration)) {
             ci.cancel();
         }
-    }
-
-    @ModifyReturnValue(method = "collectChunkBuildResults", at = @At("RETURN"))
-    private ArrayList<BuilderTaskOutput> limitBuildResults(ArrayList<BuilderTaskOutput> original) {
-        int max = MercurizerRuntimePolicy.getMaxUploadResultsPerFrame();
-        if (original.size() > max) {
-            original.subList(max, original.size()).clear();
-        }
-        return original;
-    }
-
-    @ModifyConstant(method = "updateChunks", constant = @Constant(floatValue = 0.1f, ordinal = 0))
-    private float modifyUploadFraction(float ignored) {
-        return MercurizerRuntimePolicy.getUploadFraction();
-    }
-
-    @ModifyConstant(method = "updateChunks", constant = @Constant(longValue = 2_000_000L, ordinal = 0))
-    private long modifyMinUploadBudget(long ignored) {
-        return 1_000_000L;
     }
 }
